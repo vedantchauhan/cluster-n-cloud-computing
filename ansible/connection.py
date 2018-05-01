d2f72b3d0b059214f48be827c3aa4d9654b007c1
SiddharthMalhotra <codemalhotra@gmail.com>
2018-05-02 01:55:57 +1000

    Updating Boto scripts

--- ansible/connection.py
+++ ansible/connection.py
@@ -1,5 +1,7 @@
 import boto
+
 from boto.ec2.regioninfo import RegionInfo
+from boto.ec2.connection import EC2Connection
 
 import config
 
@@ -21,6 +23,21 @@ def main():
     conn = establish()
     print(conn)
     print(conn.region)
+    #print(conn.get_region)
+    #create_instances(conn)
+
+
+def create_instances(conn, count=1):
+    # ami-000022b3 NeCTAR Ubuntu 14.04 (Trusty) amd64
+    instance_list = []
+    for c in range(count):
+        instance_list.append(
+            conn.run_instances('ami-000022b3',
+                               key_name='Default-cloud',
+                               instance_type='m1.small',
+                               security_groups=['default'],
+                               placement='melbourne-np')
+        )
 
 
 if __name__ == '__main__':
