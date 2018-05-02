bcdda9b411950257366fb1b0f61dc0795c83acd2
vedantchauhan <vedant.chauhan@gmail.com>
2018-05-02 17:01:21 +1000

    web

--- web/app.py
+++ web/app.py
@@ -23,3 +23,40 @@ def connect_db():
     server = couchdbkit.Server()
     return server.get_or_create_db(app.config['DATABASE'])
 
+
+def init_db():
+    """Creates the database views."""
+    db = connect_db()
+    loader = couchdbkit.loaders.FileSystemDocsLoader('_design')
+    loader.sync(db, verbose=True)
+
+
+class Entry(Document):
+    author = StringProperty()
+    date = DateTimeProperty()
+    title = StringProperty()
+    text = StringProperty()
+
+
+
+@app.before_request
+def before_request():
+    """Make sure we are connected to the database each request."""
+    g.db = connect_db()
+    Entry.set_db(g.db)
+
+@app.route('/')
+def show_entries():
+    # ? entries = Entry.view('entry/all')
+    entries = g.db.view('entry/all', schema=Entry)
+    #app.logger.debug(entries.all())
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.run()
